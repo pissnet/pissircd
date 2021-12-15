a969216476538481cb139ffcc967cfa9f82b5519
Ron Nazarov <noisytoot@disroot.org>
2021-12-15 19:44:26 +0000

    Make third/nosajoinpartmode send ERR_CANNOTDOCOMMAND instead of ERR_YOUREBANNEDCREEP

--- src/modules/third/nosajoinpartmode.c
+++ src/modules/third/nosajoinpartmode.c
@@ -2,6 +2,7 @@
  *   IRC - Internet Relay Chat, src/modules/third/nosajoinpartmode.c
  *   (C) 2021 Polsaker
  *   (C) 2021 chmod/Semisol
+ *   (C) 2021 Noisytoot
  *
  *   See file AUTHORS in IRC package for additional names of
  *   the programmers.
@@ -28,12 +29,13 @@ ModuleHeader MOD_HEADER
 				"third/nosajoinpartmode",
 				"1.0",
 				"Disables remote SAJOIN/SAPART targeted to local users, with umode +J",
-				"chmod, Polsaker",
+				"chmod, Polsaker, Noisytoot",
 				"unrealircd-5",
 		};
 
 
-CMD_OVERRIDE_FUNC(override_sajp);
+CMD_OVERRIDE_FUNC(override_sajoin);
+CMD_OVERRIDE_FUNC(override_sapart);
 
 long UMODE_NOSAJOINPART = 0L;
 
@@ -44,9 +46,9 @@ MOD_INIT()
 
 MOD_LOAD()
 {
-	if (!CommandOverrideAdd(modinfo->handle, "SAJOIN", override_sajp))
+	if (!CommandOverrideAdd(modinfo->handle, "SAJOIN", override_sajoin))
 		return MOD_FAILED;
-	if (!CommandOverrideAdd(modinfo->handle, "SAPART", override_sajp))
+	if (!CommandOverrideAdd(modinfo->handle, "SAPART", override_sapart))
 		return MOD_FAILED;
 	UmodeAdd(modinfo->handle, 'J', UMODE_LOCAL, 1, umode_allow_opers, &UMODE_NOSAJOINPART);
 	return MOD_SUCCESS;
@@ -57,7 +59,7 @@ MOD_UNLOAD()
 	return MOD_SUCCESS;
 }
 
-CMD_OVERRIDE_FUNC(override_sajp)
+CMD_OVERRIDE_FUNC(override_sajoin)
 {
 	Client *target;
 	if ((parc < 3) || BadPtr(parv[2]))
@@ -73,7 +75,30 @@ CMD_OVERRIDE_FUNC(override_sajp)
 	}
 	if (MyUser(target) && !MyUser(client) && (target->umodes & UMODE_NOSAJOINPART))
 	{
-		sendnumeric(client, ERR_YOUREBANNEDCREEP, "User has umode +J, and you are not on the same server as them");
+		sendnumeric(client, ERR_CANNOTDOCOMMAND, "SAJOIN", "User has umode +J, and you are not on the same server as them");
+		return;
+	}
+
+	CallCommandOverride(ovr, client, recv_mtags, parc, parv);
+}
+
+CMD_OVERRIDE_FUNC(override_sapart)
+{
+	Client *target;
+	if ((parc < 3) || BadPtr(parv[2]))
+	{
+		sendnumeric(client, ERR_NEEDMOREPARAMS, "SAPART");
+		return;
+	}
+
+	if (!(target = find_person(parv[1], NULL)))
+	{
+		sendnumeric(client, ERR_NOSUCHNICK, parv[1]);
+		return;
+	}
+	if (MyUser(target) && !MyUser(client) && (target->umodes & UMODE_NOSAJOINPART))
+	{
+		sendnumeric(client, ERR_CANNOTDOCOMMAND, "SAPART", "User has umode +J, and you are not on the same server as them");
 		return;
 	}
 
